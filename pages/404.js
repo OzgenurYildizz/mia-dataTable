6d1df6fd94e256f20bedcad7d7f1d28f799086ab
oz181 <ozgenur121212@hotmail.com>
2023-08-13 22:04:26 +0300

    first commit

---
+++ pages/404.js
@@ -0,0 +1,14 @@
+function PageNotFound(){
+
+    return(
+        <div>
+            <h1>
+                Page Not Found
+            </h1>
+        </div>
+    );
+}
+
+export default PageNotFound;
+
+/*costumes 404 page*/
---
+++ pages/index.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react"; // Gerekli importları eklediğinizden emin olun
+import axios from "axios";
+import style from "../styles/list-style.module.css";
+import CapitalIcon from "../icons/capital-icon";
+import LanguageIcon from "../icons/language-icon";
+
+export default function Home() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://restcountries.com/v3.1/all")
+      .then((response) => setCountries(response.data))
+      .catch((error) => console.log({ error }));
+  }, []);
+
+  const limitedCountries = countries.slice(0, 5);
+
+  return (
+    <div>
+      {limitedCountries.map((country) => {
+        const firstLanguageKey = Object.keys(country.languages)[0];
+        const firstLanguage = country.languages[firstLanguageKey];
+
+        return (
+          <div key={country.name.common}>
+            <ul className={style.list}>
+              <li className={style.item}>
+                <img
+                  src={country.flags.png}
+                  alt={`${country.name.common} Flag`}
+                />
+                <div className={style.content}>
+                  <div>
+                    <h2>{country.name.common}</h2>
+                    <div className={style.info}>
+                      <p>Capital City</p>
+                      <h4>
+                        <CapitalIcon /> {" "+country.capital}
+                      </h4>
+
+                      <p>Language</p>
+                      <h4>
+                        <LanguageIcon />
+                        {" "+firstLanguage}
+                      </h4>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
